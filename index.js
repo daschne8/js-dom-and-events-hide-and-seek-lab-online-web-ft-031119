2e65ed9e24aae7d3ae59e448a50393494f281ff8
Daniel Schneider <daschne8@gmail.com>
2019-06-17 21:09:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ function nestedTarget(){
   return document.querySelector('#nested .target')
 }
 function increaseRankBy(n){
-
+  const rls = document.querySelectorAll('.ranked-list')
+  for (var i = 0; i < array.length; i++) {
+    array[i]
+  }
 }
