e67c1db5d6f720eac8314578ecdf1f4cfae59135
Daniel Schneider <daschne8@gmail.com>
2019-06-17 21:23:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ function nestedTarget(){
 }
 function increaseRankBy(n){
   const rls = document.querySelectorAll(".ranked-list")
-  l = rls.length
-  for (let i = 0; i < limit; i++) {
-    rls[i] = parseInt(rls[i]) + n
+  for (let i = 0,l = rls.length ; i < l; i++) {
+    let children = rls[i].children
+
+    for (let j = 0, k = children.length; i < k; i++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
   }
 }
